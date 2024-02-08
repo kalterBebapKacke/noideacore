6894db61e5a5a7739faebb89aa65c9c28b23db3e
kalterdoner99 <kalterdoner@gmail.com>
2024-02-08 15:08:02 +0100

    New commit

---
+++ base_python/finances.py
@@ -0,0 +1,26 @@
+from . import better_SQL as s
+from . import dates
+import datetime
+
+class finaces:
+
+    def __init__(self, sql:s.SQL_Class):
+        self.sql = sql
+        self.tables = ["profit", "spendings", "stand"]
+
+    def setup(self):
+        #if tabel exists
+        pass
+
+    def new_profit(self, number:int, date=None): # id: generated, num, date)
+        if date == None:
+            date = dates.current()
+        self.sql.basic_write(self.tables[0],  profit=number, date=date)
+
+    def new_spending(self, number:int, date=None):
+        if date == None:
+            date = dates.current()
+        self.sql.basic_write(self.tables[0], profit=number, date=date)
+
+    def graph(self):
+        pass

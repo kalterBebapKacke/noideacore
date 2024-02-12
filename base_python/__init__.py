7b3552857eb947cc80b099c342ac5197c6a398a4
kalterBebapKacke <155302694+kalterBebapKacke@users.noreply.github.com>
2024-02-12 19:32:22 +0100

    Delete base_python directory

--- base_python/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import better_SQL
-from . import com
-from . import dates
-from . import encrypt
-from . import data
--- base_python/data/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import data
-from . import html
-from . import graph
